b00a33b549ac1d3f72aeac97be95cbf67fee8dc0
gazda12345kamil-dotcom <gazda12345kamil@gmail.com>
2026-03-07 13:18:04 +0100

    jj

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import ClientHomePage from "./ClientHomePage";
-import { getLatestVideos } from "../lib/youtube";
-
-export default async function Home() {
-  const videos = await getLatestVideos();
-  return <ClientHomePage videos={videos} />;
-}
